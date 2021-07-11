6e79c3c951ce13688b202db4e66985363e53b2d8
dinidimaneesha <96563@fhss.sjp.ac.lk>
2021-07-12 04:21:38 +0530

    add some buttons in home page

--- studentManagment/src/Home.java
+++ studentManagment/src/Home.java
@@ -26,20 +26,148 @@ public class Home extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        addstudent = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        oparetion1 = new javax.swing.JButton();
+        showstudent = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenuItem1 = new javax.swing.JMenuItem();
+        jMenuItem2 = new javax.swing.JMenuItem();
+        jMenu2 = new javax.swing.JMenu();
+        jMenuItem3 = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        jPanel1.setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("Student Registration System");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(208, 58, 575, 66);
+
+        addstudent.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\AddStudent.png")); // NOI18N
+        jPanel1.add(addstudent);
+        addstudent.setBounds(88, 171, 161, 137);
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel2.setText("Home");
+        jPanel1.add(jLabel2);
+        jLabel2.setBounds(20, 10, 53, 27);
+
+        oparetion1.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\configuration.png")); // NOI18N
+        jPanel1.add(oparetion1);
+        oparetion1.setBounds(361, 171, 161, 137);
+
+        showstudent.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\showStudent.png")); // NOI18N
+        jPanel1.add(showstudent);
+        showstudent.setBounds(687, 171, 152, 137);
+
+        jLabel3.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setText("Add Student");
+        jPanel1.add(jLabel3);
+        jLabel3.setBounds(88, 321, 123, 30);
+
+        jLabel4.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel4.setText("Oparetion");
+        jPanel1.add(jLabel4);
+        jLabel4.setBounds(383, 321, 101, 30);
+
+        jLabel5.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setText("Show Student");
+        jPanel1.add(jLabel5);
+        jLabel5.setBounds(711, 321, 137, 30);
+
+        jLabel6.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel6.setText("Oparetion");
+        jPanel1.add(jLabel6);
+        jLabel6.setBounds(395, 588, 101, 30);
+
+        jButton1.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\addAdmin.png")); // NOI18N
+        jPanel1.add(jButton1);
+        jButton1.setBounds(88, 438, 161, 137);
+
+        jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\configuration1.png")); // NOI18N
+        jPanel1.add(jButton2);
+        jButton2.setBounds(361, 438, 161, 137);
+
+        jButton3.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\showAdmin.png")); // NOI18N
+        jPanel1.add(jButton3);
+        jButton3.setBounds(687, 438, 161, 137);
+
+        jLabel7.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Add Admin");
+        jPanel1.add(jLabel7);
+        jLabel7.setBounds(107, 588, 110, 30);
+
+        jLabel8.setFont(new java.awt.Font("Calibri", 0, 24)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Show Admin");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(706, 584, 124, 30);
+        jPanel1.add(jLabel9);
+        jLabel9.setBounds(10, 0, 0, 0);
+
+        jLabel10.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\aaaaaaaaa\\942f93bfeae6ce40f95a7d6388ee99fb.jpg")); // NOI18N
+        jPanel1.add(jLabel10);
+        jLabel10.setBounds(0, 0, 890, 670);
+
+        jMenu1.setText("File");
+
+        jMenuItem1.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\home.png")); // NOI18N
+        jMenuItem1.setText("Home");
+        jMenu1.add(jMenuItem1);
+
+        jMenuItem2.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\logout.png")); // NOI18N
+        jMenuItem2.setText("Logout");
+        jMenu1.add(jMenuItem2);
+
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("About");
+
+        jMenuItem3.setIcon(new javax.swing.ImageIcon("C:\\Users\\user\\Desktop\\project Image\\about.png")); // NOI18N
+        jMenuItem3.setText("About");
+        jMenu2.add(jMenuItem3);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
-        pack();
+        setSize(new java.awt.Dimension(912, 746));
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     /**
@@ -78,5 +206,28 @@ public class Home extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton addstudent;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton oparetion1;
+    private javax.swing.JButton showstudent;
     // End of variables declaration//GEN-END:variables
 }
